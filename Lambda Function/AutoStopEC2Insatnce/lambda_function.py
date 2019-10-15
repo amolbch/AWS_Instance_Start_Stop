967ed221411eab92a0270fef59244fa7ad7460b9
Amol Chavan <amol@satvix.com>
2019-10-15 23:32:01 +0530

    Amol | Added AWS Instance Start/Stop Function

---
+++ Lambda Function/AutoStopEC2Insatnce/lambda_function.py
@@ -0,0 +1,9 @@
+import boto3
+# Specify the region where your insatnces are running. For example 'ap-southeast-1'
+region = 'ap-south-1'
+# Specify the insatnce IDs that you want to start at specific time. For example, ['i-abcd01234567', 'i-efgh01234567']
+instances = ['-----------']
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instances)
+    print 'started your instances: ' + str(instances)
